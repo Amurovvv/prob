a1c8ab32e4f77c82256c767bfd3143dd13ebcb7e
Amurovvv <amurov94@gmail.com>
2023-03-25 20:55:38 +0200

    neuer1

--- script.js
+++ script.js
@@ -1,47 +1,133 @@
-const userChoise = prompt('What value du you choose? \n if sum type:  +  ; \n if diff type:  -  ; \n if mult type:  *  ;\n if div type:  /  ;\n if sinus div type:  sin  ;\n if cosinus type:  cos  ;\n if pow type:  pow  ;');
+let userCondition = true;
+let userChoise;
+let firstNumber;
+let operandFirst;
+let operandSecond;
+let validMainNumber;
+let arrayHistory = [];
 
-switch (userChoise) {
-    case '+':
-        const firstNumber = +prompt('Enter the first number!');
-        const secondNumber = +prompt('Enter the second number!');
-        const sumNumbers = firstNumber + secondNumber;
-        alert(`Responce: ${sumNumbers}`);
-        break;
-    case '-':
-        const firstNumber = +prompt('Enter the first number!');
-        const secondNumber = +prompt('Enter the second number!');
-        const diffNumbers = firstNumber - secondNumber;
-        alert(`Responce: ${diffNumbers}`);
-        break;
-    case '*':
-        const firstNumber = +prompt('Enter the first number!');
-        const secondNumber = +prompt('Enter the second number!');
-        const multNumbers = firstNumber * secondNumber;
-        alert(`Responce: ${multNumbers}`);
-        break;
-    case '/':
-        const firstNumber = +prompt('Enter the first number!');
-        const secondNumber = +prompt('Enter the second number!');
-        const divNumbers = firstNumber / secondNumber;
-        alert(`Responce: ${divNumbers}`);
-        break;
-    case 'sin':
-        const firstNumber = +prompt('Enter a number!');
-        const sinValue = Math.sin(firstNumber);
-        alert(`Responce: ${sinValue}`);
-        break;
-    case 'cos':
-        const firstNumber = +prompt('Enter a number!');
-        const cosValue = Math.cos(firstNumber);
-        alert(`Responce: ${cosValue}`);
-        break;
-    case 'pow':
-        const firstNumber = +prompt('Enter the first number!');
-        const firstNumber = +prompt('Enter the first number!');
-        const powValue = Math.pow(firstNumber, secondNumber );
-        alert(`Responce: ${powValue}`);
-        break;
-    default:
-        alert('Try again');
-        break;
+function validateInputChoise() {
+    do {
+        userChoise = prompt('What value du you choose? \n if sum type:  +  ; \n if diff type:  -  ; \n if mult type:  *  ;\n if div type:  /  ;\n if sinus div type:  sin  ;\n if cosinus type:  cos  ;\n if pow type:  pow  ;\n if you want to see history of your operation, type: history');
+    } while ( userChoise !== null && userChoise !== '+' && userChoise !== '-' && userChoise !== '*' && userChoise !== '/' && userChoise !== 'sin' && userChoise !== 'cos' && userChoise !== 'pow' && userChoise !== 'history' );
 }
+
+function validateInputNumber(){
+    if(userChoise !== 'history') {
+        do {
+            firstNumber = +prompt('Enter the first number!');
+            validMainNumber = +firstNumber;
+        } while (+firstNumber !== validMainNumber);
+    }
+}
+
+function validateInputNumberSecond(){
+    if(userChoise !== 'history') {
+        do {
+            firstNumber = +prompt('Enter the first number!');
+            validMainNumber = +firstNumber;
+        } while (+firstNumber !== validMainNumber);
+    }
+}
+
+function sum(a, b) {
+    return a + b;
+}
+let sumNumbers = sum(operandFirst, operandSecond); 
+
+function diff(a, b) {
+    return a - b;
+}
+let diffNumbers = diff(operandFirst, operandSecond);
+
+function mult(a, b) {
+    return a * b;
+}
+let multNumbers = mult(operandFirst, operandSecond);
+
+function div(a, b) {
+    return a / b;
+}
+let divNumbers = div(operandFirst, operandSecond);
+
+function sin(a) {
+    return Math.sin(a);
+}
+let sinValue = sin(operandFirst);
+
+function cos(a) {
+    return Math.cos(a);
+}
+let cosValue = cos(operandFirst);
+
+function pow(a, b) {
+    return Math.pow(a, b);
+}
+let powValue = pow(operandFirst, operandSecond);
+
+
+while(userCondition) {
+    validateInputChoise();
+
+    switch (userChoise) {
+        case '+':
+            operandFirst = validateInputNumber();
+            operandSecond = validateInputNumberSecond();
+            sum(operandFirst, operandSecond);
+            arrayHistory[arrayHistory.length] = `${operandFirst} + ${operandSecond} = ${sumNumbers}`;
+            console.log(`Operation Mult finished with result ${sumNumbers}`);
+            alert(`Operation Mult finished with result ${sumNumbers}`);
+            break;
+        case '-':
+            operandFirst = validateInputNumber();
+            operandSecond = validateInputNumberSecond();
+            diff(operandFirst, operandSecond);
+            arrayHistory[arrayHistory.length] = `${operandFirst}  - ${operandSecond} = ${diffNumbers}`;
+            console.log(`Operation Diff finished with result ${diffNumbers}`);
+            break;
+        case '*':
+            operandFirst = validateInputNumber();
+            operandSecond = validateInputNumberSecond();
+            mult(operandFirst, operandSecond);
+            arrayHistory[arrayHistory.length] = `${operandFirst}  * ${operandSecond} = ${multNumbers}`;
+            console.log(`Operation Mult finished with result ${multNumbers}`);
+            break;
+        case '/':
+            operandFirst = validateInputNumber();
+            operandSecond = validateInputNumberSecond();
+            div(operandFirst, operandSecond);
+            arrayHistory[arrayHistory.length] = `${operandFirst}  / ${operandSecond} = ${divNumbers}`;
+            console.log(`Operation Div finished with result ${divNumbers}`);
+            break;
+        case 'sin':
+            operandFirst = validateInputNumber();
+            sin(operandFirst);
+            arrayHistory[arrayHistory.length] = `sin: ${operandFirst}   = ${sinValue}`;
+            console.log(`Operation Sin finished with result ${sinValue}`);
+            break;
+        case 'cos':
+            operandFirst = validateInputNumber();
+            cos(operandFirst);
+            arrayHistory[arrayHistory.length] = `cos: ${operandFirst}   = ${cosValue}`;
+            console.log(`Operation Cos finished with result ${cosValue}`);
+            break;
+        case 'pow':
+            operandFirst = validateInputNumber();
+            operandSecond = validateInputNumberSecond();
+            pow(operandFirst, operandSecond);
+            arrayHistory[arrayHistory.length] = `pow: ${operandFirst} ** ${operandSecond} = ${powValue}`;
+            console.log(`Operation Pow finished with result: ${powValue}`);
+            break;
+        case 'history':
+            console.log(arrayHistory);
+            break;
+    }
+
+    userCondition = confirm('Do you want to continue');
+
+    if(userCondition == false) {
+        alert('Thanks for your time!Have a nice day!');
+        break;
+    }
+}
+console.log('hello');
